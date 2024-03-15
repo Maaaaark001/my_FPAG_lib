86d80c27c7347dc9d3acebd92998e05e1122fe27
Maaaaark <s1251789436@163.com>
2024-03-15 10:09:48 +0800

    bug fixed

--- key_process/tb_key_process.sv
+++ key_process/tb_key_process.sv
@@ -1,28 +1,36 @@
-`timescale 1ns/10ps
+`timescale 1ns / 10ps
 `default_nettype none
 module TestKeyProcess;
-	logic clk;
-    initial begin
-        #0
-            clk=1'b0;
-        forever  #0.05  clk=~clk;
-    end
-    initial begin
-        $dumpfile("wave.vcd");
-        $dumpvars(0, TestKeyProcess );
+  logic clk;
+  initial begin
+    #0 clk = 1'b0;
+    forever #0.05  clk = ~clk;
+  end
+  initial begin
+    $dumpfile("wave.vcd");
+    $dumpvars(0, TestKeyProcess);
     #150 $finish;
+  end
+  logic key = '0, key_en;
+  initial begin
+    #10;
+    for (int i = 0; i < 30; i++) begin
+      #0.13 key = '0;
+      #0.12 key = '1;
     end
-	task automatic KeyPress(logic key, input realtime t);
-		for(int i = 0; i < 30; i++) begin
-			#0.13 key = '0; #0.12 key = '1;
-		end
-		#t; key = '0;
-	endtask
-	logic key = '0, key_en;
-	initial begin
-		#10 KeyPress(key, 50);
-		#50 KeyPress(key, 50);
-	end
-
-	KeyProcess #(100, 1) theKeyProc(clk, key, key_en);
+    #40 key = '0;
+  end
+  initial begin
+    #80;
+    for (int i = 0; i < 30; i++) begin
+      #0.13 key = '0;
+      #0.12 key = '1;
+    end
+    #40 key = '0;
+  end
+  KeyProcess #(100, 1) theKeyProc (
+      clk,
+      key,
+      key_en
+  );
 endmodule
